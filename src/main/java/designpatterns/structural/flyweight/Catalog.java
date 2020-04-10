1661e767fb2cd1791e4d8681b7fd04bf683a6eb6
jnsmelinda <jns.melinda@gmail.com>
2020-04-09 18:08:25 -0700

    flyweight pattern

---
+++ src/main/java/designpatterns/structural/flyweight/Catalog.java
@@ -0,0 +1,19 @@
+package designpatterns.structural.flyweight;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Catalog {
+    private Map<String, Item> items = new HashMap<String, Item>();
+
+    //factory method
+    public Item lookup(String itemName) {
+        if (!items.containsKey(itemName))
+            items.put(itemName, new Item(itemName));
+        return items.get(itemName);
+    }
+
+    public int totalItemsMade() {
+        return items.size();
+    }
+}
